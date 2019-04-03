e5486c9305a73af9256e32776b731c9535f7ee3c
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-03 04:43:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  #{}"SELECT author.name, sub-genre.name FROM "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series "
 end
 
 def select_character_names_and_number_of_books_they_are_in
